0cb0f02e15942a1f67bb79e75e7130d05c13af0b
Michael Golenia <michael.golenia@stud.fh-dortmund.de>
2018-07-21 12:35:37 +0200

    Initial Commit

---
+++ src/TodoList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class TodoList extends Component {
+    render() {
+        return (
+            <ul>
+                <li>Analysis 1</li>
+                <li>Programmierkurs 1</li>
+            </ul>
+        );
+    }
+}
